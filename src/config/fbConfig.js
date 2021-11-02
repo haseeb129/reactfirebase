2600feae0c2af77b7ba8b3c35c70336a65a005b7
haseeb129 <haseeb129ciit@gmail.com>
2021-11-03 00:40:36 +0500

    env added

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
 var firebaseConfig = {
-  apiKey: "AIzaSyCkJIAkg3CFivdCaHb3wbZPV2EsA69UJKU",
-  authDomain: "reactfirebase-backend-da6f3.firebaseapp.com",
-  databaseURL: "https://reactfirebase-backend-da6f3.firebaseio.com",
-  projectId: "reactfirebase-backend-da6f3",
-  storageBucket: "reactfirebase-backend-da6f3.appspot.com",
-  messagingSenderId: "889508508075",
-  appId: "1:889508508075:web:20d0afbc2d1c4fca22a2ee",
-  measurementId: "G-VHDRFKR3YQ",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
